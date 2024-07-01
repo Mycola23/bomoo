9835f08c6d5321130e64bcbadee606b6c687a114
person <litvinenkomycola263@ukr.net>
2024-07-01 16:57:21 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,1 +1,52 @@
 "use strict";
+const recorBtn = document.querySelector(".record-btn");
+const playback = document.querySelector(".main__playback");
+
+recorBtn.addEventListener("click", toggleRecord);
+
+let can_record = false;
+let is_recording = false;
+
+let recorder = null;
+let chunks = [];
+
+function setupAudio() {
+    console.log("yes setup is successful");
+    if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices
+            .getUserMedia({
+                audio: true,
+            })
+            .then(setupStream)
+            .catch((err) => {
+                console.error(err);
+            });
+    }
+}
+setupAudio();
+
+function setupStream(stream) {
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        chunks.push(e.data);
+    };
+    recorder.onstop = (e) => {
+        const blob = new Blob(chunks, { type: "audio/ogg; codecs=opus" });
+        chunks = [];
+        const audioUrl = window.URL.createObjectURL(blob);
+        playback.src = audioUrl;
+    };
+    can_record = true;
+}
+
+function toggleRecord() {
+    if (!can_record) return;
+    is_recording = !is_recording;
+    if (is_recording) {
+        recorder.start();
+        recorBtn.classList.add("__active");
+    } else {
+        recorder.stop();
+        recorBtn.classList.remove("__active");
+    }
+}
